f7de6949ff5f799d9ceb372d11ffc3e9050ed8c0
Raziel Tabib <raziel@macbook-pro-3.attlocal.net>
2019-02-11 20:03:43 -0800

    first commit

---
+++ webapi.go
@@ -0,0 +1,14 @@
+package cf_webapi_go
+
+type Client struct{
+	token string
+}
+
+func New(token string) *Client {
+	client := &Client{token:token}
+	return client
+}
+
+func (c *Client) Sum (x int,y int) int {
+	return x+y
+}git
